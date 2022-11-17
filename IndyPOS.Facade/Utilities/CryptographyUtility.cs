d9f045f985cdb39986afde01691fefe2431488d8
Purin Tavilsup <purin.tavilsup@gmail.com>
2022-11-17 14:37:10 -0600

    Refactored Cryptography Utility

--- IndyPOS.Facade/Utilities/CryptographyUtility.cs
+++ IndyPOS.Facade/Utilities/CryptographyUtility.cs
@@ -9,38 +9,34 @@ public class CryptographyUtility : ICryptographyUtility
 	private const string Hash = "b415407e-3f08-43be-8130-46f8146467f7";
 
 	public string Encrypt(string input)
-	{
-		var data = Encoding.UTF8.GetBytes(input);
-			
-		using var md5 = MD5.Create();
-		var keys = md5.ComputeHash(Encoding.UTF8.GetBytes(Hash));
+    {
+        var data = Encoding.UTF8.GetBytes(input);
+        var keys = MD5.HashData(Encoding.UTF8.GetBytes(Hash));
 
-		using var tripleDes = TripleDES.Create();
-		tripleDes.Key = keys;
-		tripleDes.Mode = CipherMode.ECB;
-		tripleDes.Padding = PaddingMode.PKCS7;
-			
-		var transform = tripleDes.CreateEncryptor();
-		var results = transform.TransformFinalBlock(data, 0, data.Length);
+        using var tripleDes = TripleDES.Create();
+        tripleDes.Key = keys;
+        tripleDes.Mode = CipherMode.ECB;
+        tripleDes.Padding = PaddingMode.PKCS7;
 
-		return Convert.ToBase64String(results, 0, results.Length);
-	}
+        var transform = tripleDes.CreateEncryptor();
+        var results = transform.TransformFinalBlock(data, 0, data.Length);
 
-	public string Decrypt(string input)
-	{
-		var data = Convert.FromBase64String(input);
+        return Convert.ToBase64String(results, 0, results.Length);
+    }
 
-		using var md5 = MD5.Create();
-		var keys = md5.ComputeHash(Encoding.UTF8.GetBytes(Hash));
+    public string Decrypt(string input)
+    {
+        var data = Convert.FromBase64String(input);
+        var keys = MD5.HashData(Encoding.UTF8.GetBytes(Hash));
 
-		using var tripleDes = TripleDES.Create();
-		tripleDes.Key = keys;
-		tripleDes.Mode = CipherMode.ECB;
-		tripleDes.Padding = PaddingMode.PKCS7;
+        using var tripleDes = TripleDES.Create();
+        tripleDes.Key = keys;
+        tripleDes.Mode = CipherMode.ECB;
+        tripleDes.Padding = PaddingMode.PKCS7;
 
-		var transform = tripleDes.CreateDecryptor();
-		var results = transform.TransformFinalBlock(data, 0, data.Length);
+        var transform = tripleDes.CreateDecryptor();
+        var results = transform.TransformFinalBlock(data, 0, data.Length);
 
-		return Encoding.UTF8.GetString(results);
-	}
+        return Encoding.UTF8.GetString(results);
+    }
 }
